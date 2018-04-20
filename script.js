78d208f6204c6ea6d82e53dabbcbb020d3026ecc
Talfaran <tal1faran@gmail.com>
2018-04-21 01:23:49 +0300

    beers finished extentions

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 var beers = [];
+var switchSort = 0;
 
-var rating = function(){
+var sortBeer = function () {
 
-}
-var renderBeers = function (){
-
-    $('.beers-list').find('li').remove();
-    for(let i = 0; i <= beers.length;i++) {
-        $('.beers-list').append('<li>' + beers[i].name +  "  rated: " + beers[i].rating  + '</li>');
+    if (switchSort === 0) {
+        beers.sort(function (a, b) { return a.rating - b.rating });
+        switchSort = 1;
+    } else if (switchSort === 1) {
+        beers.sort(function (a, b) { return b.rating - a.rating });
+        switchSort = 0;
     }
 }
 
-var addBeer = function (){
+var renderBeers = function () {
+
+    $('.beers-list').find('li').remove();
+    for (let i = 0; i < beers.length; i++) {
+        $('.beers-list').append('<li>' + beers[i].name + "  rated: " + beers[i].rating + '</li>');
+    }
+} // end of renderBeersFunction
+
+var addBeer = function () {
     var nameOfBeer = $('#nameBeer').val();
     var nameOfCategory = $('.category-input').val();
     var beerRating = $('#rating').val();
-    
+
     var beer = {
         name: nameOfBeer,
         category: nameOfCategory,
@@ -23,8 +32,9 @@ var addBeer = function (){
     };
 
     beers.push(beer);
-   
+
 }
-    
+
 $('#submitButton').click(addBeer);
 $('.render-beer').click(renderBeers);
+$('.sort-beer').click(sortBeer)
